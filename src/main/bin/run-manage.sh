f782c11edc143f4ed79fcab143461629fac336dd
尹豪 <yinhao@tuhu.cn>
2021-03-21 10:19:04 +0800

    增加本地配置文件，便于开发和线上部署 新增本地配置文件application-local.properties即可

--- src/main/bin/run-manage.sh
+++ src/main/bin/run-manage.sh
@@ -11,7 +11,7 @@ readonly PID_FILE="$LOGS_HOME/application.pid"
 readonly APP_MAIN_CLASS="jshERP.jar"
 readonly LOG_CONFIG="$CONFIG_HOME/logback-spring.xml"
 
-readonly JAVA_RUN="-Dlogs.home=$LOGS_HOME -Dlogging.config=$LOG_CONFIG -Dspring.config.location=file:$CONFIG_HOME -Dspring.pid.file=$PID_FILE -Dspring.pid.fail-on-write-error=true"
+readonly JAVA_RUN="-Dlogs.home=$LOGS_HOME -Dlogging.config=$LOG_CONFIG -Dspring.config.location=file:$CONFIG_HOME -Dspring.pid.file=$PID_FILE -Dspring.pid.fail-on-write-error=true -Dspring.profiles.active=prod"
 readonly JAVA_OPTS="-server -Xms128m -Xmx320m -XX:PermSize=128M -XX:MaxPermSize=256M $JAVA_RUN"
 
 readonly JAVA="java"
